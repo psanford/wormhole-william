bc4de80d000a7e584e5d4038d5544740acbb6501
Peter Sanford <psanford@sanford.io>
2019-08-29 14:47:30 -0700

    Add example file send/recv commands.

---
+++ examples/wormhole-william-recv-file/wormhole-william-recv-file.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/psanford/wormhole-william/wormhole"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s <code>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	code := os.Args[1]
+
+	c := wormhole.NewClient()
+
+	ctx := context.Background()
+	fileInfo, err := c.RecvFile(ctx, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("got msg: %+v\n", fileInfo)
+
+	_, err = io.Copy(os.Stdout, fileInfo)
+	if err != nil {
+		log.Fatal("readfull  error", err)
+	}
+}
---
+++ examples/wormhole-william-send-file/wormhole-william-send-file.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/psanford/wormhole-william/wormhole"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s <file>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	filename := os.Args[1]
+
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := wormhole.NewClient()
+
+	ctx := context.Background()
+	code, status, err := c.SendFile(ctx, filename, f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("On the other computer, please run: wormhole receive")
+	fmt.Printf("Wormhole code is: %s\n", code)
+
+	s := <-status
+
+	if s.Error != nil {
+		log.Fatalf("Send error: %s", s.Error)
+	} else if s.OK {
+		fmt.Println("OK!")
+	} else {
+		log.Fatalf("Hmm not ok but also not error")
+	}
+}
