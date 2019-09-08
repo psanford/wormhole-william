60f75a19baaf83803f2d42c1c494857875684290
Peter Sanford <psanford@sanford.io>
2019-09-08 13:56:42 -0700

    Reorganize files in wormhole package.
    Split by send and receive instead of by type. Move tests into a single
    file.

--- wormhole/send.go
+++ wormhole/send.go
@@ -19,6 +19,117 @@ import (
 	"golang.org/x/crypto/nacl/secretbox"
 )
 
+// SendText sends a text message via the wormhole protocol.
+//
+// It returns the nameplate+passphrase code to give to the reciever, a result chan
+// that gets written to once the reciever actually attempts to read the message
+// (either successfully or not).
+func (c *Client) SendText(ctx context.Context, msg string) (string, chan SendResult, error) {
+	sideID := crypto.RandSideID()
+	appID := c.appID()
+	rc := rendezvous.NewClient(c.url(), sideID, appID)
+
+	_, err := rc.Connect(ctx)
+	if err != nil {
+		return "", nil, err
+	}
+
+	nameplate, err := rc.CreateMailbox(ctx)
+	if err != nil {
+		return "", nil, err
+	}
+
+	pwStr := nameplate + "-" + wordlist.ChooseWords(c.wordCount())
+
+	clientProto := newClientProtocol(ctx, rc, sideID, appID)
+
+	ch := make(chan SendResult, 1)
+	go func() {
+		var returnErr error
+		defer func() {
+			mood := rendezvous.Errory
+			if returnErr == nil {
+				mood = rendezvous.Happy
+			} else if returnErr == errDecryptFailed {
+				mood = rendezvous.Scary
+			}
+
+			rc.Close(ctx, mood)
+		}()
+
+		sendErr := func(err error) {
+			ch <- SendResult{
+				Error: err,
+			}
+			returnErr = err
+			close(ch)
+			return
+		}
+
+		err = clientProto.WritePake(ctx, pwStr)
+		if err != nil {
+			sendErr(err)
+			return
+		}
+
+		err = clientProto.ReadPake()
+		if err != nil {
+			sendErr(err)
+			return
+		}
+
+		err = clientProto.WriteVersion(ctx)
+		if err != nil {
+			sendErr(err)
+			return
+		}
+
+		_, err = clientProto.ReadVersion()
+		if err != nil {
+			sendErr(err)
+			return
+		}
+
+		offer := &genericMessage{
+			Offer: &offerMsg{
+				Message: &msg,
+			},
+		}
+		err = clientProto.WriteAppData(ctx, offer)
+		if err != nil {
+			sendErr(err)
+			return
+		}
+
+		collector, err := clientProto.Collect()
+		if err != nil {
+			sendErr(err)
+			return
+		}
+		defer collector.close()
+
+		var answer answerMsg
+		err = collector.waitFor(&answer)
+		if err != nil {
+			sendErr(err)
+			return
+		}
+
+		if answer.MessageAck == "ok" {
+			ch <- SendResult{
+				OK: true,
+			}
+			close(ch)
+			return
+		} else {
+			sendErr(fmt.Errorf("Unexpected answer"))
+			return
+		}
+	}()
+
+	return pwStr, ch, nil
+}
+
 // SendFile sends a single file via the wormhole protocol. It returns a nameplate+passhrase code to give to the
 // receiver, a result channel that will be written to after the receiver attempts to read (either successfully or not)
 // and an error if one occured.
--- wormhole/wormhole_test.go
+++ wormhole/wormhole_test.go
@@ -5,14 +5,78 @@ import (
 	"bytes"
 	"context"
 	"encoding/hex"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
+	"strings"
 	"sync"
 	"testing"
 
 	"github.com/psanford/wormhole-william/rendezvous/rendezvousservertest"
 )
+
+func TestWormholeSendRecvText(t *testing.T) {
+	ctx := context.Background()
+
+	rs := rendezvousservertest.NewServer()
+	defer rs.Close()
+
+	url := rs.WebSocketURL()
+
+	// disable transit relay
+	DefaultTransitRelayAddress = ""
+
+	var c0 Client
+	c0.RendezvousURL = url
+
+	var c1 Client
+	c1.RendezvousURL = url
+
+	secretText := "Hialeah-deviltry"
+	code, statusChan, err := c0.SendText(ctx, secretText)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	nameplate := strings.SplitN(code, "-", 2)[0]
+
+	// recv with wrong code
+	msg, err := c1.Receive(ctx, fmt.Sprintf("%s-intermarrying-aliased", nameplate))
+	if err != errDecryptFailed {
+		t.Fatalf("Recv side expected decrypt failed due to wrong code but got: %s", err)
+	}
+
+	status := <-statusChan
+	if status.OK || status.Error != errDecryptFailed {
+		t.Fatalf("Send side expected decrypt failed but got status: %+v", status)
+	}
+
+	code, statusChan, err = c0.SendText(ctx, secretText)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// recv with correct code
+	msg, err = c1.Receive(ctx, code)
+	if err != nil {
+		t.Fatalf("Recv side got unexpected err: %s", err)
+	}
+
+	msgBody, err := ioutil.ReadAll(msg)
+	if err != nil {
+		t.Fatalf("Recv side got read err: %s", err)
+	}
+
+	if string(msgBody) != secretText {
+		t.Fatalf("Got Message does not match sent secret got=%s sent=%s", msgBody, secretText)
+	}
+
+	status = <-statusChan
+	if !status.OK || status.Error != nil {
+		t.Fatalf("Send side expected OK status but got: %+v", status)
+	}
+}
 
 func TestWormholeFileTransportSendRecvDirect(t *testing.T) {
 	ctx := context.Background()
