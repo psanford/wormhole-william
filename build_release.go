aa31e6b65d8eeb31384d01926351286366170714
Peter Sanford <psanford@sanford.io>
2019-12-29 08:52:46 -0800

    Add a build_release.go tool for producing binaries

---
+++ build_release.go
@@ -0,0 +1,56 @@
+// +build ignore
+
+// This is a tool to assist with building release artifacts.
+//
+// To run: go run build_release.go
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+type target struct {
+	goos  string
+	garch string
+}
+
+func (t *target) binaryName() string {
+	tmpl := "wormhole-william-%s-%s%s"
+	ext := ""
+	if t.goos == "windows" {
+		ext = ".exe"
+	}
+
+	return fmt.Sprintf(tmpl, t.goos, t.garch, ext)
+}
+
+var targets = []target{
+	{"linux", "amd64"},
+	{"linux", "arm64"},
+	{"darwin", "amd64"},
+	{"windows", "386"},
+	{"freebsd", "amd64"},
+}
+
+func main() {
+	os.MkdirAll("release", 0777)
+
+	for _, t := range targets {
+		cmd := exec.Command("go", "build", "-o", filepath.Join("release", t.binaryName()))
+		env := []string{"GOOS=" + t.goos, "GARCH=" + t.garch, "GO111MODULE=on"}
+		cmd.Env = append(os.Environ(), env...)
+
+		fmt.Printf("run: %s %s %s\n", strings.Join(env, " "), cmd.Path, strings.Join(cmd.Args, " "))
+
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s %s err: %s, out: %s\n", t.goos, t.garch, err, out)
+			os.Exit(1)
+		}
+	}
+}
