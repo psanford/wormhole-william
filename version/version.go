6f8d355f160854a6a8e3668d4347cf4194bf8ec3
Peter Sanford <psanford@sanford.io>
2023-08-16 17:28:28 -0700

    Bump version v1.0.6 => v1.0.7

--- version/version.go
+++ version/version.go
@@ -4,5 +4,5 @@ var (
 	// Default Agent identifier sent to rendezvous server
 	AgentString = "go-william"
 	// Default Agent version sent to rendezvous server
-	AgentVersion = "v1.0.6"
+	AgentVersion = "v1.0.7"
 )
